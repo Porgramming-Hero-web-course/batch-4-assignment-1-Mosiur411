7744442e59c39bd1759f7fc0f906cf176ba495e9
mosiur411 <mosiurislamwebdesign@gmail.com>
2024-11-08 17:34:20 +0600

    dist file

--- dist/problem-7.js
+++ dist/problem-7.js
@@ -1,5 +1,19 @@
 "use strict";
 {
-    //
+    //  problem 7 solved
+    class Car {
+        constructor(make, model, year) {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+        }
+        getCarAge() {
+            const currentYear = new Date().getFullYear(); // current year
+            return currentYear - this.year;
+        }
+    }
+    const car = new Car("Honda", "Civic", 2008);
+    const output = car.getCarAge();
+    console.log(output);
     //
 }
--- dist/problem-8.js
+++ dist/problem-8.js
@@ -1,5 +1,12 @@
 "use strict";
 {
-    //
+    //  problem 8 solved
+    const validateKeys = (obj, keys) => {
+        const result = keys.every(key => obj[key]);
+        return result;
+    };
+    const person = { name: "Alice", age: 25, email: "alice@example.com" };
+    const output = validateKeys(person, ["name", "age"]); //output boolean
+    console.log(output);
     //
 }
